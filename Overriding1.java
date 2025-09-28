b33927b595bd91f1a204c45928e3b7f642131889
Saranya-Techie-hub <saranyak.cs19@gmail.com>
2025-09-28 12:40:31 +0530

    Override method for the classes TV and Smart TV

---
+++ Overriding1.java
@@ -0,0 +1,40 @@
+
+package Overriding;
+
+class TV
+{
+    public void switchON() 
+    { 
+        System.out.println("TV is switched ON");
+    }
+    public void changeChannel()
+    {
+        System.out.println("TV channel is changed");
+    }
+}
+class SmartTV extends TV
+{
+    @Override
+    public void switchON()
+    {
+        System.out.println("Smart TV is switched ON");
+    }
+    @Override
+    public void changeChannel()
+    {
+       System.out.println("Smart TV channel is changed");
+    }
+    public void browse()
+    {
+        System.out.println("Smart TV browsing");
+    }
+}
+public class Overriding1 {
+
+    public static void main(String[] args) {
+        TV t = new SmartTV();
+        t.switchON();
+        t.changeChannel();
+    }
+    
+}
